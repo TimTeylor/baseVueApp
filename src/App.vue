fb58d5c36f8e226fb62314038def222aff1256cb
TimTeylor <timteillor@gmail.com>
2020-05-02 19:42:22 +0300

    vue style scoped

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div>
-    <h1>{{ carName }}</h1>
     <app-counter></app-counter>
-    <app-car 
-      :carName="carName"
-      :carYear="carYear"
-      :changeFunc="changeNameToAudi"
-      @nameChanged="carName = $event"
-      @counterUpdated="counter = $event"
-    ></app-car>
+    <app-car></app-car>
   </div>
 </template>
 
@@ -16,17 +9,6 @@
 import Car from './Car.vue'
 import Counter from './Counter.vue'
 export default {
-  data () {
-    return {
-      carName: 'Ford',
-      carYear: 2018
-    }
-  },
-  methods: {
-    changeNameToAudi() {
-      this.carName = 'Audi'
-    }
-  },
   components: {
     appCar: Car,
     appCounter: Counter
--- src/Car.vue
+++ src/Car.vue
@@ -1,48 +1,18 @@
 <template>
-  <div class="car">
-    <h3>Name: {{ carName }} \ {{ reverseName }}</h3>
-    <p>Year: {{ carYear }}</p>
-    <button @click="changeName">Change name</button>
-    <button @click="changeFunc()">Change from parent</button>
-    <button @click="updateCounter">Update counter</button>
+  <div>
+    <h2>Car</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, ipsum.</p>
   </div>
 </template>
 
 <script>
-  import {eventEmitter} from './main'
   export default {
-    props: {
-      carName: {
-        type: String,
-        default: 'Default name'
-      },
-      carYear: Number,
-      changeFunc: Function
-    },
-    methods: {
-      changeName() {
-        let name = this.carName
-        name = 'Mazda'
-        this.$emit('nameChanged', name)
-      },
-      updateCounter() {
-        eventEmitter.$emit('counterUpdated', 3)
-      }
-    },
-    computed: {
-      reverseName() {
-        return this.carName.split('').reverse().join('')
-      }
-    }
+
   }
 </script>
 
-<style>
-  .car {
+<style scoped>
+  div {
     border: 1px solid black;
-  }
-
-  .car h3 {
-    margin-bottom: 5px;
   }
 </style>
