3ec27ebe5333aa97cf571fac5ce559e28260bc3f
TimTeylor <timteillor@gmail.com>
2020-05-04 09:00:30 +0300

    vue js textarea

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h2>Form inputs</h2>
-    <input type="text" v-model.lazy="name">
-    <p>{{ name }}</p>
+    <textarea v-model="textarea"></textarea>
+
+    <p>{{ textarea }}</p>
   </div>
 </template>
 
@@ -10,12 +11,19 @@
   export default {
     data() {
       return {
-        name: 'Initial state'
+        textarea: 'I am initial text'
       }
     }
   }
 </script>
 
 <style scoped>
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
 
+  p {
+    white-space: pre;
+  }
 </style>
